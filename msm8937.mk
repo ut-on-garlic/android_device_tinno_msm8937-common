119c34f7084792fb840ad6973ddf55c2902110b0
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 18:03:05 +0300

    msm8937-common: Build thermal HAL
    Change-Id: I68be509da28339a4f9465b74ebbaff1f82687aa2

--- msm8937.mk
+++ msm8937.mk
@@ -412,6 +412,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
