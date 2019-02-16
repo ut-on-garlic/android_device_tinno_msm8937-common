02ecab82632766b6152b0a4ede6a596e00b629bc
mountaser <mountaser1998@gmail.com>
2019-02-16 18:22:28 +0530

    msm8937-common: Disable HW overlays
    * Always use GPU for screen compositing

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,6 +75,7 @@ persist.cne.feature=1
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.hw=1 \
+debug.sf.disable_hwc=1
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
