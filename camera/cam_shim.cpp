6a6710e749068a98c3966a569253a9d02cef1d5a
pbteja100 <pbteja100@gmail.com>
2021-04-03 20:38:25 +0530

    msm8937-common: camera: Add shim to fix missing symbol
    To fix - linker  : CANNOT LINK EXECUTABLE "/vendor/bin/mm-qcamera-daemon": cannot locate symbol "_ZNK6google8protobuf11MessageLite39InternalSerializeWithCachedSizesToArrayEPh" referenced by "/system/lib/libhwui.so"...

---
+++ camera/cam_shim.cpp
@@ -0,0 +1,3 @@
+namespace android {
+    extern void _ZNK6google8protobuf11MessageLite39InternalSerializeWithCachedSizesToArrayEPh() {}
+}
--- msm8937.mk
+++ msm8937.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
     camera.msm8937 \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    libshim_cam
 
 # Display
 PRODUCT_PACKAGES += \
