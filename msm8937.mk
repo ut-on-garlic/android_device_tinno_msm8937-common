b5b945addca32d971f83154d2a107f79ec49873d
pbteja100 <pbteja100@gmail.com>
2021-04-08 17:15:08 +0530

    msm8937-common: Build libjson

--- msm8937.mk
+++ msm8937.mk
@@ -304,6 +304,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
 # Qualcomm
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect
